56e603d8e5c3ee059da08b7a846e69c19722de21
Herja <anastasiazafeiropoyloy13@hotmail.com>
2022-11-17 15:36:38 +0200

    hearthstone match card setup

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
 
   
 
@@ -33,36 +35,52 @@ function App() {
 
   }
 
+  // compare 2 selected cards
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+      setDisabled(true)
+
 
       if (choiceOne.src === choiceTwo.src) {
-        console.log('those cards match')
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
         resetTurn()
       } else {
-        console.log('those cards do not match')
-        resetTurn()
+        setTimeout(() => resetTurn(), 1000)
       }
 
     }
   }, [choiceOne, choiceTwo])
+
+  console.log(cards)
 
   // reset choices & increase turn
   const resetTurn = () => {
     setChoiceOne(null)
     setChoiceTwo(null)
     setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+
   }
 
-
- 
-
+   // start new game automagically
+   useEffect(() => {
+    shuffleCards()
+  }, [])
 
   return (
     <div className="App">
 
       
-     <h1>Card Match</h1>
+     <h1>Hearthstone</h1>
+     <p>Match all the cards!</p>
      <button onClick={shuffleCards}> New Game </button>
      <div className="card-grid">
         {cards.map(card => (
@@ -70,6 +88,9 @@ function App() {
             key={card.id}
             card={card}
             handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+
           />
         ))}
       </div>
